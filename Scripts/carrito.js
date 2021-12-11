b420d28ad34abbbcfb75065974b782fc3e6bbce0
Martin Bianco <martinbianco@MacBook-Air-de-Martin.local>
2021-12-11 13:08:06 -0300

    aplicandoJavaScript

---
+++ Scripts/carrito.js
@@ -0,0 +1,38 @@
+class Carrito {
+    constructor(lista) {
+        this.lista = lista
+    }
+    findAllItems (){
+        return this.lista
+    }
+    findoneByCdItem (itemCd){
+        const item = this.lista.find(element => element.cd === itemCd);
+
+        if (!item) {
+            throw new Error(" No existe el item cd: " + itemCd)
+        }
+        return item;
+    }
+    deleteItem( itemCd ){
+        const item = this.findoneByCdItem (itemCd);
+        const index = this.lista.indexOf ( item );
+        this.lista.splice(index, 1);
+
+    } 
+}
+
+const carrito = new Carrito(
+    [LemonPie,TortaBrownie,Talitas,PancitosSaborizados]
+)
+
+/* Comprobacion de los metodos */
+
+console.log(carrito.findAllItems())
+
+carrito.deleteItem(1)
+
+console.log(carrito.findAllItems())
+
+carrito.deleteItem(9)
+
+
